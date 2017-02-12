14e1ee9aa7a168ebb531c4a1b4f27b76c6df2f8f
francisypl <francisypl@gmail.com>
2017-02-12 11:08:08 -0800

    started skeleton for story.js

---
+++ api/controllers/story.js
@@ -0,0 +1,19 @@
+const _ = require('underscore');
+
+function getStories(req, res) {
+
+}
+
+function insertNewStories(req, res) {
+
+}
+
+function voteOnStory(req, res) {
+
+}
+
+module.exports = {
+    getStories,
+    insertNewStories,
+    voteOnStory
+};
