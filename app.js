155ae1622136af092c91e749c585b8b6bb37a068
francisypl <francisypl@gmail.com>
2017-02-11 17:58:36 -0800

    fixed style issues

--- app.js
+++ app.js
@@ -7,18 +7,16 @@ var app = restify.createServer();
 module.exports = app; // for testing
 
 var config = {
-  appRoot: __dirname // required config
+    appRoot: __dirname // required config
 };
 
 SwaggerRestify.create(config, function(err, swaggerRestify) {
-  if (err) { throw err; }
+    if (err) {
+        throw err;
+    }
 
-  swaggerRestify.register(app);
+    swaggerRestify.register(app);
 
-  var port = process.env.PORT || 10010;
-  app.listen(port);
-
-  if (swaggerRestify.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
+    var port = process.env.PORT || 10010;
+    app.listen(port);
 });
